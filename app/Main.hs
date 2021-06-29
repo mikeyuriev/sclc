4e2ec14e9d9aef4a8bd5b934ac54e144b7650cf2
Mike Yuriev <mikeyuriev@gmail.com>
2021-06-29 13:24:51 +0300

    Add graphical representation of the error

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,33 @@ module Main where
 
 import Control.Monad.State (unless)
 import System.IO (hFlush, stdout)
-import Text.Parsec
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Pos as PP
 import SmallCalc.Eval
 import SmallCalc.Parser
 
 main :: IO ()
 main = loop (Right 0.0)
 
-loop :: Either ParseError Float -> IO ()
+loop :: Either P.ParseError Float -> IO ()
 loop value = do
     putResult value
     putStr ">"
     hFlush stdout
     input <- getLine
-    unless (null input) $ loop $ eval <$> parse line "" input
+    unless (null input) $ loop $ evalLine "User input" input
 
-putResult :: Either ParseError Float -> IO ()
+putResult :: Either P.ParseError Float -> IO ()
 putResult (Right value) = print value
-putResult (Left err)    = putStrLn $ "Error " ++ show err
+putResult (Left err)    = putStrLn $ "E" ++ graphicErrorPos (errorPos err)
 
+evalLine :: P.SourceName -> String -> Either P.ParseError Float
+evalLine source s = eval <$> P.parse line source s
+
+errorPos :: P.ParseError -> Int
+errorPos = PP.sourceColumn . P.errorPos
+
+graphicErrorPos :: Int -> String
+graphicErrorPos 0 = ""
+graphicErrorPos 1 = "^"
+graphicErrorPos x = '-' : graphicErrorPos (x - 1)
