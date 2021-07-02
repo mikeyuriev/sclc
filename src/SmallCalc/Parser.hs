0aef6d7a6b1c5ce7b81b1f99403ba555f556ce41
Mike Yuriev <mikeyuriev@gmail.com>
2021-07-02 16:27:40 +0300

    Refactor Parser.hs

--- src/SmallCalc/Parser.hs
+++ src/SmallCalc/Parser.hs
@@ -13,57 +13,50 @@ line :: Parser Node
 line = expr <* eof
 
 expr :: Parser Node
-expr = skipSpaces *> term `chainl1` addOp
+expr =  many space *> term `chainl1` lexeme addOp
 
 term :: Parser Node
-term = factor `chainl1` mulOp
+term = factor `chainl1` lexeme mulOp
 
 factor :: Parser Node
-factor = (unaryOp <*> factor) <|> parens <|> number
+factor = (lexeme unaryOp <*> factor) <|> lexeme parens <|> lexeme number
 
-skipSpaces :: Parser ()
-skipSpaces = skipMany space
+lexeme :: Parser a -> Parser a
+lexeme p = p <* skipMany space
 
 parens :: Parser Node
 parens =
     between oParen cParen expr
-    <* skipSpaces
     where
         oParen = char '(' <?> "open parenthesis '('"
         cParen = char ')' <?> "closing parenthesis ')'"
 
 number :: Parser Node
-number = Value . Constant <$> fmap read num <* skipSpaces
+number = Value . Constant <$> fmap read num
     where
         num :: Parser String
-        num = (++) <$> integer <*> decimal
-        integer :: Parser String
-        integer = digits
+        num = (++) <$> digits <*> decimal <?> "number"
+
         decimal :: Parser String
         decimal = option "" $ (:) <$> dot <*> digits
+
         digits :: Parser String
         digits = many1 (digit <?> "digit")
         dot :: Parser Char
+
         dot = char '.' <?> "decimal dot '.'"
 
 addOp :: Parser (Node -> Node -> Node)
 addOp =
-    (
         (char '+' $> BinaryOp Add <?> "addition operator '+'")
     <|> (char '-' $> BinaryOp Sub <?> "subtraction operator '-'")
-    )
-    <* skipSpaces
 
 mulOp :: Parser (Node -> Node -> Node)
 mulOp =
-    (
         (char '*' $> BinaryOp Mul <?> "multiplication operator '*'")
     <|> (char '/' $> BinaryOp Div <?> "division operator '/'")
     <|> (char '%' $> BinaryOp Mod <?> "modulo operator '%'")
-    )
-    <* skipSpaces
 
 unaryOp :: Parser (Node -> Node)
 unaryOp =
     (char '-' <?> "unary minus operator '-'") $> UnaryOp Negate
-    <* skipSpaces
