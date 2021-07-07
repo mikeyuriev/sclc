b41968babc1cc41061bce43a5a284ab00540d589
Mike Yuriev <mikeyuriev@gmail.com>
2021-07-07 21:37:31 +0300

    Refactor Eval.hs

--- src/SmallCalc/Eval.hs
+++ src/SmallCalc/Eval.hs
@@ -1,33 +1,36 @@
 module SmallCalc.Eval
-    ( execStatement
-    , evalExpr
+    ( evalExpr
+    , execStatement
     ) where
 
+import Control.Monad (join, liftM2)
 import Data.Fixed (mod')
 import SmallCalc.Context
 import SmallCalc.Types
 
-binaryOp :: BinaryOp -> Either Error Number -> Either Error Number -> Either Error Number
-binaryOp Div _ (Right 0) = Left EDivisionByZero
-binaryOp Mod _ (Right 0) = Left EDivisionByZero
-binaryOp Add x y = fmap (+) x <*> y
-binaryOp Sub x y = fmap (-) x <*> y
-binaryOp Mul x y = fmap (*) x <*> y
-binaryOp Div x y = fmap (/) x <*> y
-binaryOp Mod x y = fmap mod' x <*> y
+binaryOp :: BinaryOp -> Number -> Number -> Either Error Number
+binaryOp Div _ 0 = Left EDivisionByZero
+binaryOp Mod _ 0 = Left EDivisionByZero
+binaryOp op  x y = Right (operation op x y)
+    where
+        operation Add = (+)
+        operation Sub = (-)
+        operation Mul = (*)
+        operation Div = (/)
+        operation Mod = mod'
 
-unaryOp :: UnaryOp -> Either Error Number -> Either Error Number
-unaryOp Negate = fmap (* (-1.0))
+unaryOp :: UnaryOp -> Number -> Either Error Number
+unaryOp Negate = Right . negate
 
 evalExpr :: Context -> Expr -> Either Error Number
-evalExpr _ (Constant c)
-    = pure c
-evalExpr ctx (UnaryOp op e)
-    = unaryOp op (evalExpr ctx e)
-evalExpr ctx (BinaryOp op le re)
-    = binaryOp op (evalExpr ctx le) (evalExpr ctx re)
-evalExpr ctx (Symbol name)
-    = getSymbol ctx name
+evalExpr _   (Constant c)        = Right c
+evalExpr ctx (UnaryOp op e)      = unaryOp op =<< evalExpr ctx e
+evalExpr ctx (BinaryOp op le re) = join $ liftM2 o l r
+    where
+        o = binaryOp op
+        l = evalExpr ctx le
+        r = evalExpr ctx re
+evalExpr ctx (Symbol name)       = getSymbol ctx name
 
 execStatement :: Context -> Statement -> Either Error Context
 execStatement ctx@(Context syms _) (Out expr)
